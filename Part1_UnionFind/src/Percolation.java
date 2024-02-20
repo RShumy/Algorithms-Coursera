d82fd0c2c64a2c297103b9cd77714e8ded911d87
RShumy <razvansuhan@yahoo.com>
2024-02-20 03:56:04 +0200

    Fixed allowing a site to be opened mere than once Inverted parameter call to Math.pow(n,2)

--- Part1_UnionFind/src/Percolation.java
+++ Part1_UnionFind/src/Percolation.java
@@ -1,9 +1,6 @@
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
-
 public class Percolation {
     // I will try to flatten the 2d array into a 1d array
     //
@@ -14,7 +11,7 @@ public class Percolation {
     // making a row, column index check transposed into 1d:
     // 1D array index = row * totalColumns + column
 
-    private int[] grid;
+    private boolean[] grid;
     private WeightedQuickUnionUF unionFindGrid;
     private int edgeLength;
     private int array1DLength;
@@ -22,110 +19,109 @@ public class Percolation {
 
     // percolation would work in the context of a 2d square matrix,
     // but we will work with a flattened version
-    public Percolation( int n ) throws IllegalArgumentException{
+    public Percolation(int n) {
         if (n <= 0) exception("Cont construct a matrix of length below or equal to 0");
         this.edgeLength = n;
         this.array1DLength = n*n;
-        grid = new int[ array1DLength ];
-        for ( int i=0; i< array1DLength; i++ ){
+        grid = new boolean[ array1DLength ];
+        for (int i = 0; i < array1DLength; i++) {
             // initializing the grid to 0
-            grid[i] = 0;
-            this.unionFindGrid = new WeightedQuickUnionUF( array1DLength+1 );
+            grid[i] = false;
+            this.unionFindGrid = new WeightedQuickUnionUF(array1DLength+1);
         }
     }
 
-    private void exception(String message) throws IllegalArgumentException{
+    private void exception(String message) {
         throw new IllegalArgumentException(message);
     }
     // made to apply to natural index assignment from 1 to n, not from 0 to n-1
-    private int index(int row, int column ){
+    private int index(int row, int column) {
         // subtracting 1 from row to adjust for flatten index
-        if (isOutOfBounds(row,column))
-            exception(String.format("Row %d or Column %d is out of Bounds for Length %d",row,column,edgeLength));
+        if (isOutOfBounds(row, column))
+            exception(String.format("Row %d or Column %d is out of Bounds for Length %d", row, column, edgeLength));
         return (row-1) * edgeLength + (column-1);
     }
 
-    public void open( int row, int column ){
-        int index = index( row, column );
-        grid[index] = 1;
-        openSites++;
+    public void open(int row, int column) {
+        int index = index(row, column);
+        if (!grid[index]) openSites++;
+        grid[index] = true;
         if (row == 1 && column <= edgeLength)
             fillSite(0,index);
     }
 
-    public boolean isOpen( int row, int column ){
-        return isOpen(index(row,column));
+    public boolean isOpen(int row, int column) {
+        return isOpen(index(row, column));
     }
 
     private boolean isOpen (int flattenIndex) {
-        if (flattenIndex == array1DLength) return grid[flattenIndex-1] == 1;
-        return grid[flattenIndex] == 1;
+        if (flattenIndex == array1DLength) return grid[flattenIndex-1];
+        return grid[flattenIndex];
     }
     
-    public boolean isFull( int row, int column){
+    public boolean isFull( int row, int column) {
         if (!isOutOfBounds(row, column))
-            return isFull(index(row,column));
+            return isFull(index(row, column));
         else return false;
     }
 
-    private boolean isFull(int flattenIndex){
+    private boolean isFull(int flattenIndex) {
         if (flattenIndex == array1DLength) return unionFindGrid.find(flattenIndex) == 0;
         return unionFindGrid.find(flattenIndex+1) == 0;
     }
 
-    private boolean validToFill(int previousFlattenIndex,int row, int column){
+    private boolean validToFill(int previousFlattenIndex, int row, int column) {
         if (previousFlattenIndex == array1DLength) previousFlattenIndex--;
-        if (!isOutOfBounds(row,column))
-            return isFull(previousFlattenIndex) && isOpen(row,column) && !isFull(row,column);
+        if (!isOutOfBounds(row, column))
+            return isFull(previousFlattenIndex) && isOpen(row, column) && !isFull(row, column);
         return false;
     }
 
-    private void fillSite( int flattenIndex, int flattenIndexNeighbour ){
+    private void fillSite( int flattenIndex, int flattenIndexNeighbour ) {
         if (flattenIndex != 0)
             unionFindGrid.union(flattenIndex+1, flattenIndexNeighbour+1);
         else unionFindGrid.union(0,flattenIndexNeighbour+1);
     }
 
-    private void tryToFill (int previousFlatIndex,int row, int column) throws InterruptedException {
-        int index = index(row,column);
-        if (validToFill(previousFlatIndex,row,column)) {
-            fillSite(previousFlatIndex,index);
+    private void tryToFill (int previousFlatIndex, int row, int column) {
+        int index = index(row, column);
+        if (validToFill(previousFlatIndex, row, column)) {
+            fillSite(previousFlatIndex, index);
         }
-        if ( validToFill(index,row-1,column) ) tryToFill(index,row-1,column);
-        if ( validToFill(index,row+1,column) ) tryToFill(index,row+1,column);
-        if ( validToFill(index,row,column-1 )) tryToFill(index, row,column-1);
-        if ( validToFill(index,row,column+1) ) tryToFill(index, row,column+1);
+        if ( validToFill(index, row+1, column) ) tryToFill(index, row+1, column);
+        if ( validToFill(index, row-1, column) ) tryToFill(index, row-1, column);
+        if ( validToFill(index, row,column-1 )) tryToFill(index, row, column-1);
+        if ( validToFill(index, row,column+1) ) tryToFill(index, row, column+1);
     }
 
     /** returns true if index is out of bounds */
-    private boolean isOutOfBounds(int row, int column){
+    private boolean isOutOfBounds(int row, int column) {
         return row < 1 || row > edgeLength || column < 1 || column > edgeLength;
     }
 
-    public int numberOfOpenSites(){
+    public int numberOfOpenSites() {
         return this.openSites;
     }
 
-    public boolean percolates() throws InterruptedException {
-        for (int col=1; col<=edgeLength;col++) if(isFull(1,col))
-                tryToFill(index(1,col),2,col);
+    public boolean percolates() {
+        for (int col=1; col<=edgeLength;col++)
+            if(isFull(1,col))
+                tryToFill(index(1, col), 2, col);
         for (int col=1; col<=edgeLength; col++)
-            if (isFull(edgeLength,col)) return true;
+            if (isFull(edgeLength, col)) return true;
         return false;
     }
 
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
 
         Percolation p = new Percolation(25);
         for (int i = 1; i <= p.array1DLength - (p.array1DLength / 7); i++)
             p.open(StdRandom.uniformInt(1, p.edgeLength + 1), StdRandom.uniformInt(1, p.edgeLength + 1));
         System.out.println(p.percolates());
         p.print2DArray();
-        Percolation a = new Percolation(60);
     }
-    private void print2DArray() throws InterruptedException {
-        TimeUnit.MILLISECONDS.sleep(100);
+    private void print2DArray() {
         // USING ANSI escape codes to print colored text
         String CYAN = "\u001B[36m";
         String RED = "\u001B[31m";
@@ -133,10 +129,10 @@ public class Percolation {
         String CYAN_BKGRND = "\u001B[48;5;45m";
         String WHT_BKGRND = "\u001B[48;5;195m";
         String BLCK_BKGRND = "\u001B[48;5;16m";
-        for (int row=1; row<=edgeLength; row++){
+        for (int row=1; row<=edgeLength; row++) {
             for (int col=1; col<=edgeLength; col++) {
                 if (isFull(row, col)) System.out.print( CYAN_BKGRND + "   " );
-                else if (!isFull(row, col) && isOpen(row,col)) System.out.print( WHT_BKGRND + "   " );
+                else if (!isFull(row, col) && isOpen(row, col)) System.out.print( WHT_BKGRND + "   " );
                 else System.out.print( BLCK_BKGRND + "   " );
                 System.out.print(ANSI_RESET);
             }
--- Part1_UnionFind/src/PercolationStats.java
+++ Part1_UnionFind/src/PercolationStats.java
@@ -2,40 +2,41 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
-   double[] trials;
+   private double[] trials;
+   private static final double CONFIDENCE_95 = 1.92;
 
-    public PercolationStats(int n, int trials) throws InterruptedException {
+    public PercolationStats(int n, int trials) {
         this.trials = new double[trials];
         for (int trial=0; trial<trials; trial++){
             Percolation percolationTrial = new Percolation(n);
-            for (boolean percolates = false; !percolates; percolates = percolationTrial.percolates()){
-                    int row = StdRandom.uniformInt(1,n+1);
-                    int col = StdRandom.uniformInt(1,n+1);
-                    if(!percolationTrial.isOpen(row, col))
-                        percolationTrial.open(row,col);
-                }
+            while (!percolationTrial.percolates()) {
+                int row = StdRandom.uniformInt(1,n+1);
+                int col = StdRandom.uniformInt(1,n+1);
+                if(!percolationTrial.isOpen(row, col))
+                    percolationTrial.open(row,col);
+            }
             //trial fraction
-            this.trials[trial]= percolationTrial.numberOfOpenSites() / Math.pow(2,n);
+            this.trials[trial]= percolationTrial.numberOfOpenSites() / Math.pow(n,2);
         }
     }
 
-    public double mean(){
+    public double mean() {
         return StdStats.mean(trials);
     }
 
-    public double stddev(){
+    public double stddev() {
         return StdStats.stddev(trials);
     }
 
-    public double confidenceLo(){
-        return mean() - ((1.96 * stddev()) / Math.sqrt(trials.length));
+    public double confidenceLo() {
+        return mean() - (CONFIDENCE_95 * stddev() / Math.sqrt(trials.length));
     }
 
-    public double confidenceHi(){
-        return mean() + ((1.96 * stddev()) / Math.sqrt(trials.length));
+    public double confidenceHi() {
+        return mean() + (CONFIDENCE_95 * stddev() / Math.sqrt(trials.length));
     }
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         PercolationStats ps = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
 
         String confidence = ps.confidenceLo() + ", " + ps.confidenceHi();
